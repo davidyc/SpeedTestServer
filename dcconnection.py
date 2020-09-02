39a808b3e08348efc9978e8b9f3f6cb8e75ec09b
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2020-09-02 14:30:00 +0600

    add value for all parametrs

--- dcconnection.py
+++ dcconnection.py
@@ -11,28 +11,38 @@ config = {
 
 conn = mariadb.connect(**config)
 cur = conn.cursor()
+_keyid = "keyid"
+_dateandtime = "dateandtime"
+_typetest = "typetest"
+_valuetest = "valuetest"
 
 
 def initTable(dbname):
     try:
-        cur.execute("CREATE TABLE {} (keyid VARCHAR(50), dateandtime VARCHAR(45),  typetest VARCHAR(15), valuetest VARCHAR(45))".format(dbname))
+        cur.execute("CREATE TABLE {} ({} VARCHAR(50), {} VARCHAR(45),{} VARCHAR(15), {} VARCHAR(45))"
+        .format(dbname, _keyid,_dateandtime,_typetest,_valuetest))
     except Exception as e:
         print(str(e))
 
 def getallrowbytype(dbname, typetest):
-    cur.execute("select * from {} where typetest = '{}'".format(dbname, typetest))
-    row_headers=[x[0] for x in cur.description]
-    rv = cur.fetchall()
-    rows =[]
-    for row in rv:
-        rows += [{"keyid":row[0], "dateandtime":row[1], "typetest":row[2], "valuetest":row[3]}]
-    return json.dumps(rows, sort_keys=True, indent=4, separators=(',', ': '))
+    cur.execute("select * from {} where {} = '{}'".format(dbname, _typetest, typetest))
+    return _getjsonresponse(cur)
 
 def getallrowbykey(dbname, typetest, keyid):
-    cur.execute("select * from {} where keyid = '{}' and typetest = '{}' ".format(dbname, keyid, typetest))
-    row_headers=[x[0] for x in cur.description]
-    rv = cur.fetchall()
+    cur.execute("select * from {} where {} = '{}' and {} = '{}'"
+    .format(dbname, _keyid, keyid, _typetest, typetest))
+    return _getjsonresponse(cur)
+
+def addnewrow(dbname,keyid, timecurrent, typetest, value):
+    sql = "INSERT INTO {} ({}, {}, {}, {}) VALUES (%s, %s, %s, %s)".format(dbname, _keyid, _dateandtime, _typetest, _valuetest)
+    val = (keyid, timecurrent, typetest, value)
+    cur.execute(sql, val)
+    conn.commit()
+
+def _getjsonresponse(cursor):
+    row_headers=[x[0] for x in cursor.description]
+    rv = cursor.fetchall()
     rows =[]
     for row in rv:
-        rows += [{"keyid":row[0], "dateandtime":row[1], "typetest":row[2], "valuetest":row[3]}]
-    return json.dumps(rows, separators=(',', ': '))
+        rows += [{_keyid:row[0], _dateandtime:row[1], _typetest:row[2], _valuetest:row[3]}]
+    return json.dumps(rows, sort_keys=True, indent=4, separators=(',', ': '))
